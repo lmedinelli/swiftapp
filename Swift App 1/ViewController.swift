5944b7361112b62802016de13b9e3da9185b59cf
Luis Medinelli <lmedinelli@azurian.com>
2017-02-20 00:32:09 -0300

    Initial Commit

---
+++ Swift App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 1
+//
+//  Created by Luis Medinelli on 2/20/17.
+//  Copyright © 2017 iBeacon Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
